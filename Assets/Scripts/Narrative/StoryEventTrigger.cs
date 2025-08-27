e2a3b0dd2a15ebea76735903d3fdaf6b5d419709
giulio-arecco <giulioarecco@gmail.com>
2025-08-27 20:29:25 +0200

    Refactor StoryEventTrigger to support multiple Ink files
    StoryEventTrigger now accepts a list of Ink JSON assets and triggers them sequentially using a queue.

--- Assets/Scripts/Narrative/StoryEventTrigger.cs
+++ Assets/Scripts/Narrative/StoryEventTrigger.cs
@@ -1,9 +1,19 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class StoryEventTrigger : MonoBehaviour {
-    [SerializeField] private TextAsset inkJson;
+    [SerializeField] private List<TextAsset> inkJsonList;
+
+    private Queue<TextAsset> _inkJsonQueue;
+
+    private void Awake() {
+        _inkJsonQueue = new Queue<TextAsset>(inkJsonList);
+    }
 
     public void TriggerStoryEvent() {
-        StoryManager.Instance.EnterStoryEvent(inkJson);
+        if (_inkJsonQueue.Count > 0)
+            StoryManager.Instance.EnterStoryEvent(_inkJsonQueue.Dequeue());
+        else
+            Debug.LogError("Empty inkJson queue");
     }
 }
